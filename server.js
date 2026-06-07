7ce84cd91bc1cd872309e391d56b8ce0e8b6300f
golamrabbi73 <golamrabbi39hr@gmail.com>
2026-06-07 15:11:24 +0600

    added get car api, post car api, get single car

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const dontenv = require("dotenv")
-const { MongoClient, ServerApiVersion } = require('mongodb');
-dontenv.config()
-
-const uri =process.env.MONGODB_URI
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
+
+console.log("MONGODB_URI:", process.env.MONGODB_URI);
+console.log("PORT:", process.env.PORT);
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const uri = process.env.MONGODB_URI;
 
 const app =express();
 
@@ -38,14 +40,50 @@ async function run() {
     const carsCollection = db.collection("cars");
     const bookingsCollection = db.collection("bookings");
 
+    //get cars api
+    app.get("/cars", async (req, res) => {
+        try{
+            const result = await carsCollection.find().toArray();
+            res.send(result);
+        }   catch(error){
+            res.status(500).send({ message: "Failed to get cars" });
+        }
+    });
+
+    // post car api
+    app.post("/cars", async(req, res) => {
+        try{
+            const car = req.body;
+            const result = await carsCollection.insertOne(car);
+            res.send(result);
+        } catch(error){
+            res.status(500).send({message: "Failed to add car"});
+        }
+    });
+
+    // get single car
+    app.get("/cars/:id", async(req, res) => {
+        try{
+            const id = req.params.id;
+
+            const result = await carsCollection.findOne({
+                _id: new ObjectId(id),
+            });
+
+            res.send(result);
+        }   catch(error){
+            res.status(500).send({message: "Car not found"});
+        }
+    });
+
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    await client.close();
+  } catch(error){
+    console.log(error);
   }
 }
-run().catch(console.dir);
+run()
 
 app.get("/", (req, res) => {
     res.send("DriveFleet server Running");
